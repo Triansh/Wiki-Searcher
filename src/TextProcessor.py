85d8ae7999644446a859838fe6604ce65ebe0518
Triansh <trianshsharma@gmail.com>
2021-08-26 22:54:35 +0530

    some redundant word removals

--- src/TextProcessor.py
+++ src/TextProcessor.py
@@ -1,64 +1,71 @@
 import config
 import re
-from nltk.corpus import stopwords
 from Stemmer import Stemmer
 
 
 class TextProcessor(object):
     def __init__(self):
         self.stemmer = Stemmer('english')
-        self.stop_words = set(
-            stopwords.words('english') + config.punctuations + config.extra_stop_words)
+        self.stop_words = set(config.extra_stop_words)
 
         self.token_regex = re.compile(config.token_regex)
         self.infobox_regex = re.compile(config.infobox_regex)
         self.category_regex = re.compile(config.category_regex)
         self.link_regex = re.compile(config.links_regex)
         self.reference_regex = re.compile(config.reference_regex)
+
         self.garbage_regex = re.compile(config.garbage_regex)
+        self.ignore_regex = re.compile(config.ignore_ref_regex)
+        self.http_regex = re.compile(config.http_regex)
 
         self.doc_map = {}
+
+    def remove_pattern(self, content):
+        content = self.ignore_regex.sub(' ', content)
+        content = self.http_regex.sub(' ', content)
+        return content
 
     def processDoc(self, doc):
         self.doc_map = {}
-        title = doc['title']
-        content = doc['text'].lower()
-        self.cleanup(content, '', False)
-        self.extract_title(title.lower())
+        title = doc['title'].lower()
+        content = self.remove_pattern(doc['text'].lower())
+
+        self.cleanup(content + title, '', False)
+        self.extract_title(title)
         self.extract_infobox(content)
         self.extract_categories(content)
         self.extract_links(content)
-        self.extract_references(content + title.lower())
+        self.extract_references(content)
         return self.doc_map
 
     # Extract titles and don't stem it
     def extract_title(self, content):
         title = content.strip()
-        texts = self.cleanup(title, 't', True, False)
+        self.cleanup(title, 't', True, False)
 
     # Remove infoboxes to avoid redundant content
     def extract_infobox(self, content):
         splits = [x.end() for x in self.infobox_regex.finditer(content)]
-        texts = [self.cleanup(content[index:(index + x)], 'i') for index in splits if
-                 (x := content[index:].find('\n}}')) != -1]
+        all(self.cleanup(content[index:(index + x)], 'i') for index in splits if
+            (x := content[index:].find('\n}}')) != -1)
 
     # Remove Category to avoid redundant content
     def extract_categories(self, content):
         splits = [x.end() for x in self.category_regex.finditer(content)]
-        texts = [self.cleanup(content[index:(index + x)], 'c') for index in splits if
-                 (x := content[index:].find(']]')) != -1]
+        all(self.cleanup(content[index:(index + x)], 'c') for index in splits if
+            (x := content[index:].find(']]')) != -1)
 
     # Remove reference tags and https to avoid redundant content
     def extract_references(self, content):
         splits = [x.end() for x in self.reference_regex.finditer(content)]
-        texts = [self.cleanup(content[index:(index + x)], 'r') for index in splits if
-                 (x := content[index:].find('</ref>')) != -1]
+        all(self.cleanup(content[index:(index + x)], 'r') for index in splits if
+            (x := content[index:].find('</ref>')) != -1)
 
     # Remove https and cite to avoid redundant content
     def extract_links(self, content):
         splits = [x.end() for x in self.link_regex.finditer(content)]
-        texts = [self.cleanup(content[index:(index + x)], 'l') for index in splits if
-                 (x := content[index:].find('\n\n')) != -1]
+        all(self.cleanup(content[index:(index + x)], 'l') for index in splits if
+            (x := content[index:].find('\n\n')) != -1)
 
     def cleanup(self, content, add_tag, reduce=True, stem=True):
 
@@ -67,8 +74,10 @@ class TextProcessor(object):
         if reduce:
             stem_sentence = set(stem_sentence)
         stem_sentence = [x for x in stem_sentence if
-                         len(x) > 2 and (not self.garbage_regex.match(x)) and
-                         not (x[0] in '0123456789' and len(x) > 4)]
+                         len(x) > 1
+                         and (not self.garbage_regex.match(x))
+                         # and not (x[0] in '0123456789' and len(x) > 4)
+                         ]
         for token in stem_sentence:
             if token in self.doc_map:
                 if add_tag == "":
@@ -79,4 +88,4 @@ class TextProcessor(object):
                 self.doc_map[token] = [1, '']
 
         # return " ".join(stem_sentence)
-        return ''
+        return True

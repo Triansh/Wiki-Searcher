49b64d80254fade58d53102097564f80cc118c83
Triansh <trianshsharma@gmail.com>
2021-09-08 20:22:02 +0530

    Basic searching code done

--- src/Ranker.py
+++ src/Ranker.py
@@ -1,4 +1,31 @@
+from math import log
+
+
 class Ranker(object):
 
     def __init__(self):
-        pass
+        self.k1 = 1.2
+        self.b = 0.75
+        self.total_docs = 52612  # 21384756
+        self.avg_doc_len = 20000
+        self.idf_scores = {}
+        self.word_count = {}
+        self.word_to_doc_map = {}
+        self.final_score = {}
+        self.doc_size = {}
+        self.results = []
+
+    def calculate_scores(self):
+        self.idf_scores = {k: self.get_idf(v) for k, v in self.word_count.items()}
+        for w, d in self.word_to_doc_map.items():
+            for doc_data in d:
+                self.final_score[doc_data[1]] += self.get_score(w, doc_data[1], doc_data[0])
+        self.results = [k for k, v in
+                        sorted(self.final_score.items(), key=lambda x: x[1], reverse=True)[:10]]
+
+    def get_score(self, word, doc_id, word_freq):
+        return (self.idf_scores[word] * word_freq * (self.k1 + 1) / (word_freq + (
+                self.k1 * (1 - self.b + (self.b * self.doc_size[doc_id] / self.avg_doc_len)))))
+
+    def get_idf(self, val):
+        return log(1 + ((self.total_docs - val + 0.5) / (val + 0.5)))
--- src/search.py
+++ src/search.py
@@ -1,42 +1,37 @@
-import json
-import time
-import os, sys
-import re
-from Stemmer import Stemmer
+import os
 import pickle
+import re
+import sys
+import time
+from Stemmer import Stemmer
+
+from Ranker import Ranker
 
 
-class QueryProcessor(object):
+class QueryProcessor(Ranker):
 
-    def __init__(self, path_to_index):
+    def __init__(self, path_to_index_dir, query_string):
 
+        super().__init__()
         self.stemmer = Stemmer('english')
         with open(os.path.join(os.getcwd(), 'src/stopwords.pkl'), 'rb') as f:
             self.stop_words = set(pickle.load(f))
 
-        self.path_to_index = os.path.join(os.getcwd(), path_to_index)
-        self.index_files, self.title_files, self.index_heads = [], [], []
-        self.get_all_files()
+        self.query_string = query_string.strip()
 
-        self.stem_words = set()
-        self.mapper = {}
+        self.path_to_index = os.path.join(os.getcwd(), path_to_index_dir)
+        with open(os.path.join(self.path_to_index, 'heads.txt'), 'r') as f:
+            self.index_heads = [x.rstrip() for x in f.readlines()]
+
+        self.max_file_lines = 10 ** 4
+
         self.curr_query = {}
 
-        self.tags = set(list("tbircle"))
         self.field_regex = re.compile(r"[tbircl]:")
 
-    def get_all_files(self):
-        self.index_files = [open(os.path.join(self.path_to_index, filename), 'r')
-                            for filename in os.listdir(self.path_to_index)
-                            if filename.startswith('index_')]
-        self.title_files = [open(os.path.join(self.path_to_index, filename), 'r')
-                            for filename in os.listdir(self.path_to_index)
-                            if filename.startswith('title_')]
-        with open('heads.txt', 'r') as f:
-            self.index_heads = [x.strip() for x in f.readlines()]
-
+    # Binary search on index heads to to get the file no.
     def search_file(self, word):
-        s, e, ans = 0, len(self.index_files), 0
+        s, e, ans = 0, len(self.index_heads) - 1, 0
         while s <= e:
             mid = (s + e) // 2
             if self.index_heads[mid] > word:
@@ -46,45 +41,110 @@ class QueryProcessor(object):
                 ans = mid
         return ans
 
-    def extract_words(self, query_string):
-        indices = [x.start() for x in self.field_regex.finditer(query_string.strip())]
+    # Parsing the query into broken words if the tags they require
+    # self.curr_query is of the form {word : tags}
+    def extract_words(self):
+        indices = [x.start() for x in self.field_regex.finditer(self.query_string)]
         if len(indices) > 0:
             if indices[0] != 0: indices = [0] + indices
-            parts = [query_string[i:j].strip() for i, j in zip(indices, indices[1:] + [None])]
+            parts = [self.query_string[i:j].strip() for i, j in zip(indices, indices[1:] + [0])]
         else:
-            parts = [query_string]
-
-        for x in parts:
-            tag = 'e'
-            if x[1] == ':':
-                tag, x = x[0], x[2:]
-            for y in x[2:].split():
+            parts = [self.query_string.strip()]
+        # print(parts)
+        for q in parts:
+            tag, x = (q[0], q[2:]) if q[1] == ':' else ('bcilrt', q)
+            # print(tag, x)
+            for y in x.split():
                 if y == '' or y in self.stop_words: continue
+                y = self.stemmer.stemWord(y)
                 if y in self.curr_query:
                     self.curr_query[y] += tag
                 else:
                     self.curr_query[y] = tag
-                    self.mapper[y] = self.stemmer.stemWord(y)
-        self.curr_query = {k: ''.join(set(list(v))) for k, v in self.curr_query.items()}
-        self.stem_words = set(self.mapper.values())
+        self.curr_query = {k: ''.join(set(v)) for k, v in self.curr_query.items()}
+        # print(self.curr_query)
 
-    def process_query(self, query_string):
-        self.extract_words(query_string)
-        # self.load_file_data()
+    # This get all the required data from the index files
+    # word count contains in how many documents the word came. Form :- {word : count}
+    # word doc map contains the list of doc_id and frequency of the word in that document.
+    # Form :- {word : [(frequency, doc_id)]}
+    def get_index_data(self):
+        word_file_map = {}
+        for tok, tags in self.curr_query.items():
+            file_no = self.search_file(tok)
+            if file_no in word_file_map:
+                word_file_map[file_no].add(tok)
+            else:
+                word_file_map[file_no] = {tok}
+
+        for file_no, words in word_file_map.items():
+            with open(os.path.join(self.path_to_index, f'index_{file_no}.txt'), 'r') as f:
+                word_docs = {z[0]: [tuple(x.split(',') + ['b'])
+                                    for x in z[1].split()] for x in f.readlines()
+                             if (z := x.split(':') or True) and z[0] in words}
+                word_docs = {k: [(int(x[0]), int(x[1])) for x in v
+                                 if any(z in (x[2] or 'b') for z in self.curr_query[k])] for k, v in
+                             word_docs.items()}
+                self.word_count.update({k: len(v) for k, v in word_docs.items()})
+                self.word_to_doc_map.update(word_docs)
+
+    # This gets the field length of all the required documents contained in doc_size
+    # Form :- {doc_id : doc_size}
+    def get_doc_len_data(self):
+        doc_ids = set(y[1] for doc in self.word_to_doc_map.values() for y in doc)
+        print(sorted(doc_ids))
+        self.final_score = {x: 0 for x in doc_ids}
+        doc_file_map = {}
+        for x in doc_ids:
+            q, r = (x - 1) // self.max_file_lines, (x - 1) % self.max_file_lines
+            if q in doc_file_map:
+                doc_file_map[q].add(r)
+            else:
+                doc_file_map[q] = {r}
+
+        print(doc_file_map)
+
+        for file_no, lines in doc_file_map.items():
+            with open(os.path.join(self.path_to_index, f'freq_{file_no}.txt')) as f:
+                file = f.readlines()
+                self.doc_size.update({(self.max_file_lines * file_no + 1 + line): int(file[line])
+                                      for line in lines})
+        print(self.doc_size)
+
+    def process_query(self):
+        self.extract_words()
+        self.get_index_data()
+        self.get_doc_len_data()
+        self.calculate_scores()
+        self.get_results()
+        print(self.final_score)
         # self.getResults()
 
-    def getResults(self):
-        pass
-        # with open('output.json', 'w') as f:
-        # print(json.dumps(self.result, indent=4))
+    def get_results(self):
+        file_map, title_map = {}, {}
+        for x in self.results:
+            q, r = (x - 1) // self.max_file_lines, (x - 1) % self.max_file_lines
+            if q in file_map:
+                file_map[q].add(r)
+            else:
+                file_map[q] = {r}
+
+        for f_no, lines in file_map.items():
+            with open(os.path.join(self.path_to_index, f'title_{f_no}.txt')) as f:
+                file = f.readlines()
+                title_map.update(
+                    {(self.max_file_lines * f_no + 1 + li): file[li].rstrip() for li in lines})
+
+        self.results = [(x, title_map[x]) for x in self.results]
+        print('\n'.join(str(x[0]) + ', ' + x[1] for x in self.results))
 
 
 if __name__ == '__main__':
     start = time.time()
     path_to_index = sys.argv[1]
     query = sys.argv[2].lower().replace(',', ' ')
-    qp = QueryProcessor(path_to_index)
-    qp.process_query(query)
+    qp = QueryProcessor(path_to_index, query)
+    qp.process_query()
     print("Time taken: ", time.time() - start)
 
     pass
